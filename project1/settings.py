650eecb7954ceb53f6d168e510458ef6f5d0d81b
Daniel <leonarddaniel99@gmail.com>
2023-05-25 15:45:36 +0530

    Photo Added

--- project1/settings.py
+++ project1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,5 @@ EMAIL_HOST_USER = 'livewiretbm@gmail.com'
 EMAIL_HOST_PASSWORD = 'hello123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+STATIC_FILE_DIRS = [os.path.join(BASE_DIR,'static')]
